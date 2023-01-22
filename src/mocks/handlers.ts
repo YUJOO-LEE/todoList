493434993bf8c140dcfff4888f002d3f4255420d
YUJOO-LEE <lllllllllee@gmail.com>
2023-01-23 07:52:06 +0900

    handlers 용도 별 주석 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,6 +4,7 @@ import { todoStore } from './data/service';
 import type { Todo, AddPayload, PutPayload, TodoFilters, ResPagingData, ResAllData } from './types/todo';
 
 export const handlers = [
+  // GET : 데이터 리스트 반환 (페이징)
   rest.get('/api/todo', async (req, res, ctx) => {
     try {
       const offset = getNumberValueFromURLSearchParams({
@@ -32,6 +33,8 @@ export const handlers = [
       return res(ctx.status(400), ctx.json({ message: 'no task' }));
     }
   }),
+
+  // POST : 신규 데이터 등록
   rest.post('/api/todo', async (req, res, ctx) => {
     const body: AddPayload = await req.json();
     try {
@@ -45,17 +48,19 @@ export const handlers = [
       return res(ctx.status(400), ctx.json({ message: 'error' }));
     }
   }),
+
+  // PUT : edit Mode에서 데이터 전체 수정
   rest.put('/api/todo', async (req, res, ctx) => {
     const body: PutPayload = await req.json();
     try {
-      const prevData = await todoStore.getTodo(body.id);
+      const prevTodo = await todoStore.getTodo(body.id);
 
-      if (!prevData) {
+      if (!prevTodo) {
         return res(ctx.status(400), ctx.json({ message: 'error' }));
       }
 
       const curDate = new Date().toISOString();
-      const todo: Todo = Object.assign(prevData, body, { updatedAt: curDate })
+      const todo: Todo = Object.assign(prevTodo, body, { updatedAt: curDate })
 
       await todoStore.setTodo(body.id, todo);
       return res(ctx.status(201), ctx.json({ todo }));
@@ -63,6 +68,8 @@ export const handlers = [
       return res(ctx.status(400), ctx.json({ message: 'error' }));
     }
   }),
+
+  // PATCH : 작업 완료 체크 시 데이터 일부 수정
   rest.patch('/api/todo', async (req, res, ctx) => {
     const { id, isCompleted }: { id: string, isCompleted: boolean } = await req.json();
     try {
@@ -70,6 +77,7 @@ export const handlers = [
       if (!prevTodo) {
         return res(ctx.status(400), ctx.json({ message: 'No Item' }));
       }
+
       const curDate = new Date().toISOString();
       const todo: Todo = Object.assign(prevTodo, { isCompleted, updatedAt: curDate })
 
@@ -79,24 +87,32 @@ export const handlers = [
       return res(ctx.status(400), ctx.json({ message: 'error' }));
     }
   }),
+
+  // DELETE : 데이터 삭제
   rest.delete('/api/todo', async (req, res, ctx) => {
-    const id = req.url.searchParams.get('id');
-    if (!id) {
+    try {
+      const id = req.url.searchParams.get('id');
+      if (!id) {
+        return res(ctx.status(400), ctx.json({ message: 'error' }));
+      }
+      const curData = await todoStore.getTodo(id);
+      const { data } = await todoStore.getAllTodos();
+  
+      await Promise.all(data.map(async (item: Todo) => {
+        let newTags = item.tags.replace(curData!.id + curData!.title + ',', '');
+        newTags = newTags.replace(curData!.id + curData!.title, '');
+        const todo: Todo = Object.assign(item, { tags: newTags })
+        todoStore.setTodo(item.id, todo);
+      }));
+  
+      await todoStore.removeTodo(id);
+      return res(ctx.status(201));
+    } catch {
       return res(ctx.status(400), ctx.json({ message: 'error' }));
     }
-    const curData = await todoStore.getTodo(id);
-    const { data } = await todoStore.getAllTodos();
-
-    await Promise.all(data.map(async (item: Todo) => {
-      let newTags = item.tags.replace(curData!.id + curData!.title + ',', '');
-      newTags = newTags.replace(curData!.id + curData!.title, '');
-      const todo: Todo = Object.assign(item, { tags: newTags })
-      todoStore.setTodo(item.id, todo);
-    }));
-
-    await todoStore.removeTodo(id);
-    return res(ctx.status(201));
   }),
+
+  // GET : 페이징 없이 모든 데이터 반환 (참조 리스트 출력용)
   rest.get('/api/todo/all', async (req, res, ctx) => {
     try {
       const { data, total } = await todoStore.getAllTodos();
